31f9e23b140306403913552134699c7fe4451006
Paul Maddox <prmadd02@louisville.edu>
2015-02-12 11:55:01 -0500

    Create StudentRecordClient.java

---
+++ Programming-Challenge-1/Problem/cecs/example/StudentRecordClient.java
@@ -0,0 +1,46 @@
+package cecs.example;
+
+// We need to include our resources from the 'cecs' and 'cecs.io' packages; alternatively, we could include use the
+//	cecs.* notation, which would include all the classes in the cecs package and in subpackages.
+import java.util.ArrayList;
+
+import cecs.StudentRecord;
+import cecs.io.StudentRecordSource;
+
+public class StudentRecordClient {
+
+	public static void main(String[] args) {
+		
+		StudentRecordSource rs = new StudentRecordSource("students.txt");
+		
+		// Example using the StudentRecordSource API; this will print the StudentRecords
+		//	(from the beginning of the resultset to the 25th entry) to the terminal
+		int i = 1;
+		for(StudentRecord sr : rs.getRecords(0,25))
+		{
+			if(sr != null) {
+				System.out.println(i + " " + sr.getName());
+				System.out.println("-----------");
+				i++;
+			}
+		}
+		
+		
+		// Using the StudentRecordSource API (i.e. the methods
+		//	getNumberOfRecords() and getRecords(int, int))  I want you to
+		//	iterate over the entire record set, displaying 10 records at a time.
+		//  Note you may also parameterize this constant.
+		//  
+		//	As a user of the StudentRecordSource API, assume that you have no 
+		// 	knowledge of the implementation details or the sample data.
+		//
+		//
+		// This task is a common in real-world settings, whether iterating a record
+		//  set from a database, or paginating a list of articles; you'll need
+		//  to do perform this task.
+		
+			
+	}
+
+	
+}
