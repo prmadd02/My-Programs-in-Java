09a9151b7840868fe53073283b1238d4ced24d09
Paul Maddox <prmadd02@louisville.edu>
2015-02-12 12:37:24 -0500

    Update StudentRecordClient.java
    Cleaned up Code

--- Programming-Challenge-1/Solution/StudentRecordClient.java
+++ Programming-Challenge-1/Solution/StudentRecordClient.java
@@ -1,8 +1,5 @@
 package cecs.example;
 
-// We need to include our resources from the 'cecs' and 'cecs.io' packages; alternatively, we could include use the
-//	cecs.* notation, which would include all the classes in the cecs package and in subpackages.
-
 import cecs.StudentRecord;
 import cecs.io.StudentRecordSource;
 
@@ -12,39 +9,17 @@ public class StudentRecordClient {
 		
 		StudentRecordSource rs = new StudentRecordSource("students.txt");
 		
-		// Example using the StudentRecordSource API; this will print the StudentRecords
-		//	(from the beginning of the resultset to the 25th entry) to the terminal
-		/*int i = 1;
-		for(StudentRecord sr : rs.getRecords(0,25))
-		{
-			if(sr != null) {
-				System.out.println(i + " " + sr.getName());
-				System.out.println("-----------");
-				i++;
-			}
-		}*/
-		
-		
-		// Using the StudentRecordSource API (i.e. the methods
-		//	getNumberOfRecords() and getRecords(int, int))  I want you to
-		//	iterate over the entire record set, displaying 10 records at a time.
-		//  Note you may also parameterize this constant.
-		//  
-		//	As a user of the StudentRecordSource API, assume that you have no 
-		// 	knowledge of the implementation details or the sample data.
-		//
-		//
-		// This task is a common in real-world settings, whether iterating a record
-		//  set from a database, or paginating a list of articles; you'll need
-		//  to do perform this task.
-	
+		// initialized veriables
 		int batchSize = 10;
 		int numRecords = rs.getNumberOfRecords();
+		
+		// created a true/false ? operator to cover the possibly that not all names would end up in batches
 		int numBatches = (numRecords % batchSize) > 0 ? (numRecords/batchSize) + 1 : (numRecords/batchSize);
 		
 		int i = 0;
 		
 		while(i < numBatches) {
+			
 			
 			int limit = batchSize;
 			
