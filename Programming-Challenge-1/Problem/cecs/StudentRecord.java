433b72e7cf569e427b0dc348bd31b4ac9e064bba
Paul Maddox <prmadd02@louisville.edu>
2015-02-12 11:51:16 -0500

    Create StudentRecord.java

---
+++ Programming-Challenge-1/Problem/cecs/StudentRecord.java
@@ -0,0 +1,24 @@
+package cecs;
+
+public class StudentRecord {
+	
+	private int id;
+	private String name;
+
+	public StudentRecord(String name) {
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getId() {
+		return Integer.toString(id);
+	}
+}
